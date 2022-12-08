e8edbea870cac1170e54adb6d5060194d784a073
Bunmi Oke <itisbunmioke@gmail.com>
2022-12-07 19:40:16 -0700

    Fetched images via API

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
+const getImage = async function () {
+  // The fetch URL was assigned to a variable ('imageUrl') first as TypeScript plugin(?) of VS Code was flagging the two forward slashes ('//') of the URL.
+  const imageUrl = `https://picsum.photos/v2/list?limit=100`;
+  const res = await fetch(imageUrl);
+  const images = await res.json();
+  console.log(images);
+};
+
+getImage();
